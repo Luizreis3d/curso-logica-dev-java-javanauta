88ee1cb9020ce8a039af9a9a15eefa3aa8dc9312
Luizreis3d <luizreis.3d@gmail.com>
2025-10-24 15:53:27 -0300

    atualização do projeto

--- src/modulo02_estruturasCondicionais/aula03_ifElse.java
+++ src/modulo02_estruturasCondicionais/aula03_ifElse.java
@@ -20,17 +20,30 @@ public class aula03_ifElse {
 //            System.out.println("Vamos ficar em casa e pedir pizza");
 //        }
 
-        boolean temDinheiro = true;
-        boolean temCartao = false;
+//        boolean temDinheiro = true;
+//        boolean temCartao = false;
+//
+//        if (temDinheiro && temCartao) {
+//            System.out.println("Pede um ifood e um zé delivery");
+//        }
+//        else if (temDinheiro || temCartao) {
+//            System.out.println("Pede um ifood");
+//        }
+//        else {
+//            System.out.println("Não pede nada");
+//        }
 
-        if (temDinheiro && temCartao) {
-            System.out.println("Pede um ifood e um zé delivery");
+        int idade = 17;
+        int idadeMinimaParaVotar = 18;
+
+        if (idade >= idadeMinimaParaVotar) {
+            // Condição (17 >= 18) é FALSA
+            System.out.println("Você pode votar!");
+        } else {
+            // Este bloco é executado
+            System.out.println("Você ainda não pode votar.");
         }
-        else if (temDinheiro || temCartao) {
-            System.out.println("Pede um ifood");
-        }
-        else {
-            System.out.println("Não pede nada");
-        }
+        // Saída: Você ainda não pode votar.
+
     }
 }
