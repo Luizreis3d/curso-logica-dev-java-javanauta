f8e36eb6ae8b64444417fa51a6e773de88029d99
Luizreis3d <luizreis.3d@gmail.com>
2025-10-26 21:07:18 -0300

    atualização dos estudos de lógica

---
+++ src/modulo03_estruturasRepetitivas/aula01_while.java
@@ -0,0 +1,12 @@
+package modulo03_estruturasRepetitivas;
+
+public class aula01_while {
+    public static void main(String[] args) {
+        int contador = 0;
+
+        while(contador<=5) {
+            System.out.println("contador: " + contador );
+            contador++;
+        }
+    }
+}
