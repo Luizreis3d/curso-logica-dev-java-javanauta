1138f400c29cd4480edfbeb312cbf952be065ee2
Luizreis3d <luizreis.3d@gmail.com>
2025-10-28 10:35:16 -0300

    atualização dos exercícios

--- src/modulo03_estruturasRepetitivas/aula05_estudoArrayList.java
+++ src/modulo03_estruturasRepetitivas/aula05_estudoArrayList.java
@@ -15,5 +15,20 @@ public class aula05_estudoArrayList {
         for (String item : listaDeCompras){
             System.out.println(item);
         }
+
+        String item = listaDeCompras.get(3);
+        System.out.println("Na posição três eu tenho: " + item);
+
+//      listaDeCompras.remove(3)
+        boolean eVazia = listaDeCompras.isEmpty();
+        int tamanho = listaDeCompras.size();
+        boolean contem = listaDeCompras.contains("Arroz");
+        System.out.println("A lista está vazia? " + eVazia + ", Qual o tamanho da lista? "
+                + tamanho + ", A lista contem o item arroz? " + contem);
+
+//        listaDeCompras.clear();
+//        System.out.println("A lista está vazia? " + listaDeCompras.isEmpty());
+
+        listaDeCompras.forEach(produto -> System.out.println(produto));
     }
 }
